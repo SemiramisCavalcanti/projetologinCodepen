dae30b828b1ec172f9fb0999d768f54f45347554
PROG WEB - MANHA <PROG WEB - MANHA@MININT-QVG78E3>
2021-01-07 10:56:15 -0200

    Login Codepen -

---
+++ index.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>projeto01</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="plugins/css/login.css"/>
+
+    </head>
+
+
+    <body>
+        <div class="container text-center mx-auto mt-5">
+            <di class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="plugins/img/001.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Login 01</h5>
+                            <p class="card-text">CodePen Home
+                               <br /> Login Form
+                               <br /> Tyler Fry
+                            </p>
+                            <a href="paginas/login01.php" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="plugins/img/002.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Login 02</h5>
+                            <p class="card-text">CodePen Home
+                                <br /> Day 001 Login Form
+                                <br /> Mohan Khadka</p>
+                            <a href="paginas/login02.php" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="http://placehold.it/300x400" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Login 03</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="paginas/login03.php" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                </div>
+
+            </di>
+        </div>
+
+
+
+
+    </body>
+
+</html>
---
+++ paginas/login01.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>projeto01</title>
+        <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../plugins/css/login01.css"/>
+
+    </head>
+
+    <body>
+        
+        <div class="login">
+            <h1>Login</h1>
+            <form method="post">
+                <input type="text" name="u" placeholder="Username" required="required" />
+                <input type="password" name="p" placeholder="Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+            </form>
+            <br />
+            <div class="text-center">
+            <a href="../index.php" class="mt-2"><button type="submit" class="btn btn-primary btn-sm">Home</button></a>      
+            </div>
+        </div>
+        
+        
+    </body>
+    
+</html>
+
+
+
+
+
+
+
+
+
